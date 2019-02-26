10c4b9ef2cb64d01e678f812d27d523e5947d1c0
Olavi Haapala <olavi.haapala@futurice.com>
2019-02-26 18:41:46 +0200

    Change the text

--- src/Main.elm
+++ src/Main.elm
@@ -1,23 +1,28 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), init, main, update, view, viewResults)
 
 import Browser
-import Html exposing (Html, text, div, h1, textarea, input)
+import Html exposing (Html, div, h1, input, text, textarea)
 import Html.Attributes exposing (type_, value)
 import Html.Events exposing (onInput)
+
+
 
 ---- MODEL ----
 
 
 type alias Model =
     { text : String
-    , results: Maybe (List String) }
+    , results : Maybe (List String)
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
     ( { text = ""
-        , results = Nothing
-      }, Cmd.none )
+      , results = Nothing
+      }
+    , Cmd.none
+    )
 
 
 
@@ -31,10 +36,8 @@ type Msg
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-
         TextChange text ->
             ( { model | text = text }, Cmd.none )
-
 
 
 
@@ -44,17 +47,23 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text "Miks – Create random mixes of people" ]
-         , textarea [ value model.text, onInput TextChange ][]
-         , div [][ input [ type_ "submit", value "Submit" ][]]
-         , viewResults model.results
+        [ h1 [] [ text "Miks –\u{00A0}Create random teams of a group of people" ]
+        , textarea [ value model.text, onInput TextChange ] []
+        , div [] [ input [ type_ "submit", value "Submit" ] [] ]
+        , viewResults model.results
         ]
 
-viewResults: Maybe (List String) -> Html msg
+
+viewResults : Maybe (List String) -> Html msg
 viewResults results =
     case results of
-        Just(list) -> div [] [text "result"]
-        _ -> div [] [text "nada"]
+        Just list ->
+            div [] [ text "result" ]
+
+        _ ->
+            div [] [ text "Not implemented" ]
+
+
 
 ---- PROGRAM ----
 
