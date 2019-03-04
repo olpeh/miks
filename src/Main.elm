6013807850a7304ea163b062f2bbe00047604cb8
Olavi Haapala <olavi.haapala@futurice.com>
2019-03-04 20:04:22 +0200

    Add team splitting

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
-module Main exposing (Model, Msg(..), init, main, update, view, viewResults)
+module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Html, div, h1, input, text, textarea)
+import Html exposing (Html, div, h1, input, li, text, textarea, ul)
 import Html.Attributes exposing (placeholder, type_, value)
 import Html.Events exposing (onClick, onInput, onSubmit)
 import Random
@@ -23,14 +23,16 @@ placeholderContent =
 
 type alias Model =
     { text : String
-    , results : List String
+    , persons : List String
+    , teamCount : Int
     }
 
 
 init : ( Model, Cmd Msg )
 init =
     ( { text = placeholderContent
-      , results = []
+      , persons = []
+      , teamCount = 2
       }
     , Cmd.none
     )
@@ -42,8 +44,9 @@ init =
 
 type Msg
     = TextChange String
+    | TeamCountChange String
     | Submit
-    | Results (List String)
+    | Shuffled (List String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -52,18 +55,27 @@ update msg model =
         TextChange text ->
             ( { model | text = text }, Cmd.none )
 
+        TeamCountChange str ->
+            case String.toInt str of
+                Just int ->
+                    ( { model | teamCount = int }, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
         Submit ->
             ( model
-            , Random.generate Results (mapInputToResults model.text)
+            , Random.generate Shuffled (shuffleNames model.text)
             )
 
-        Results results ->
-            ( { model | results = results }, Cmd.none )
+        Shuffled persons ->
+            ( { model | persons = persons }, Cmd.none )
 
 
-mapInputToResults : String -> Random.Generator (List String)
-mapInputToResults text =
+shuffleNames : String -> Random.Generator (List String)
+shuffleNames text =
     text
+        |> String.trim
         |> String.split ","
         |> Random.List.shuffle
 
@@ -77,23 +89,30 @@ view model =
     div []
         [ h1 [] [ text "Miks –\u{00A0}Create random teams of a group of people" ]
         , textarea [ value model.text, onInput TextChange, placeholder placeholderContent ] []
-        , div [] [ input [ type_ "submit", value "Submit", onClick Submit ] [] ]
-        , viewResults model.results
+        , div [] [ input [ type_ "number", value (String.fromInt model.teamCount), onInput TeamCountChange ] [] ]
+        , div [] [ input [ type_ "submit", value "Shuffle!", onClick Submit ] [] ]
+        , div [] (List.map viewTeam <| split (List.length model.persons // model.teamCount) model.persons)
         ]
 
 
-viewResults : List String -> Html msg
-viewResults results =
-    div []
-        [ results
-            |> List.map viewResult
-            |> div []
-        ]
+split : Int -> List a -> List (List a)
+split i list =
+    case List.take i list of
+        [] ->
+            []
+
+        listHead ->
+            listHead :: split i (List.drop i list)
 
 
-viewResult : String -> Html msg
-viewResult res =
-    div [] [ text res ]
+viewTeam : List String -> Html msg
+viewTeam team =
+    ul [] (List.map viewTeamMember team)
+
+
+viewTeamMember : String -> Html msg
+viewTeamMember person =
+    li [] [ text person ]
 
 
 
